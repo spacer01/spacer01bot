741ee457b16daa1e753ab976d866e234a6d299ce
spacer01 <spacer01@gmail.com>
2018-02-11 18:56:22 -0500

    make it better

--- local_settings.py
+++ local_settings.py
@@ -11,7 +11,7 @@ MY_ACCESS_TOKEN_SECRET = 'cj62He01EuNO0r9m42LkVScMM9RgAivJolw1AsuOzG7Is'
 # Sources (Twitter, local text file or a web page)
 SOURCE_ACCOUNTS = ["spacer01"]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
-STATIC_TEST = True  # Set this to True if you want to test Markov generation from a static file instead of the API.
+STATIC_TEST = False  # Set this to True if you want to test Markov generation from a static file instead of the API.
 TEST_SOURCE = "testcorpus.txt"  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
 SCRAPE_URL = False  # Set this to true to scrape a webpage.
 SRC_URL = ['http://www.example.com/one', 'https://www.example.com/two']  # A comma-separated list of URLs to scrape
